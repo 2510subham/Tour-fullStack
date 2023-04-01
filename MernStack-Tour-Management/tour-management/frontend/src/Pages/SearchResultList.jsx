fb2e71f0de8647411cd189559f0bf84b4145caec
Subham Agarwal <subhamagarwal400@gmail.com>
2023-04-01 13:31:15 +0530

    searchbar navigate done

--- MernStack-Tour-Management/tour-management/frontend/src/Pages/SearchResultList.jsx
+++ MernStack-Tour-Management/tour-management/frontend/src/Pages/SearchResultList.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import CommonSection from '../Shared/CommonSection';
 import { Container, Row, Col } from 'reactstrap';
 import { useLocation } from 'react-router-dom';
 import TourCard from '../Shared/TourCard';
+import NewsLetter from '../Shared/NewsLetter';
+///  this page comming afetr hitting this -> tours/search?city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize}
+//by the help of uselocation we can break the url into 
+//**  pathname=/tours/search  , search= city=${location}&distance=${distance}&maxGroupSize=${maxGroupSize} and state=[] is the array */
 
 const SearchResultList = () => {
   const location = useLocation(); // location is an object inside this we get state and pathname
-  const { state } = location;
+  // console.log(location);
+  const [data] = useState(location.state);//we are extracting state array from the location object
+  // console.log(data);
 
   return (
     <>
@@ -15,12 +21,21 @@ const SearchResultList = () => {
       <section>
         <Container>
           <Row>
-            <Col>
             
-            </Col>
+                {
+                  data.length===0 ? (<h1 className='text-center'>No Tour Found</h1>) : 
+                  (data?.map((item) => (
+                    <Col lg='3' className='mb-4'>
+                      <TourCard tour={item} key={item._id}/>
+                    </Col>
+                    )
+                  ))
+                }
+            
           </Row>
         </Container>
       </section>
+      <NewsLetter/>
     </>
   )
 }
